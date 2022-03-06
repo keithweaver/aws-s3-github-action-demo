8e915eed5932f184042a1d9e1a8ec0c2617d896e
keithweaver <keithmweaver@gmail.com>
2022-03-06 09:24:40 -0500

    Dev

---
+++ verify.py
@@ -0,0 +1,10 @@
+# Requirements:
+# - Verify file exists (for cp not mv)
+# - Verify file exists and old removed (for mv)
+# - Verify file does not exist (for rm)
+# - Verify folder exists and all subfiles (for cp)
+def main():
+    print ('Hello world')
+
+
+main()

42310fb7bcf540a4960dc46199bee411097b09db
keithweaver <keithmweaver@gmail.com>
2022-03-06 09:37:55 -0500

    Fixed moved

--- verify.py
+++ verify.py
@@ -3,6 +3,9 @@
 # - Verify file exists and old removed (for mv)
 # - Verify file does not exist (for rm)
 # - Verify folder exists and all subfiles (for cp)
+# - Verify folder exists and all subfiles and old removed (for mv)
+# - Verify folder does not exist (for rm)
+
 def main():
     print ('Hello world')
 
